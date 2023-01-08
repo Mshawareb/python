89c0c9cf8ab2e12c53b4bac70c4dd32417395e31
Mshawareb <79672898+Mshawareb@users.noreply.github.com>
2023-01-08 20:25:07 +0200

    Add files via upload

---
+++ first python project.py
@@ -0,0 +1,11 @@
+name = "shawareb"
+company = "daltex for agricultural development"
+age = "23"
+job = "accounts receivable accountant"
+college = "business administration "
+print("my name is " + name)
+print("i work in " + company)
+print("i am " + age + "years old")
+print(company + " like its employees to be in this age")
+print("when i was young i dreamed about working in " + company + "and when i graduated from "+college + "\n i worked "
+                                                                                                        "there "+job)
